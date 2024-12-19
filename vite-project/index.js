b212270adeaca42247b376a153b27d9f616c2b57
Nick Schirripa <108542730+NickSchirripa@users.noreply.github.com>
2024-12-19 18:43:51 -0500

    changed api search
    changed  api to look for  dragons and pretzels

--- vite-project/index.js
+++ vite-project/index.js
@@ -1,14 +1,27 @@
-document.getElementById("btn").addEventListener("click", function () {
-  fetch(
-    "https://api.unsplash.com/photos/random/?client_id=fJ0M9egBXvaO6gEgCjvmyMAwl1EBZ1tJS18xYfcwq3I"
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      const imageUrl = data.urls.full;
-      console.log(imageUrl);
+fetch(
+  "https://api.unsplash.com/photos/random?query=pretzels&query=dragon&client_id=fJ0M9egBXvaO6gEgCjvmyMAwl1EBZ1tJS18xYfcwq3I"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    const imageUrl = data.urls.full;
+    const name = `${data.user.first_name} ${data.user.last_name}` 
+    console.log(name);
 
-      document.getElementById(
-        "body"
-      ).style.backgroundImage = `url(${imageUrl})`;
-    });
-});
+    document.getElementById(
+      "body"
+    ).style.backgroundImage = `url(${imageUrl})`;
+
+    document.getElementById("artist").innerHTML = `<p>${name}</p>`
+
+
+  });
+
+  function displayLocalTime() {
+    const now = new Date(); 
+    const localTime = now.toLocaleTimeString(); 
+    document.getElementById("localTime").innerText = `${localTime}`;
+}
+
+
+displayLocalTime();
+setInterval(displayLocalTime, 1000);
